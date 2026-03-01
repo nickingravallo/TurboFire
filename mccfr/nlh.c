2f98e961050e6703f956d49322b28feb0e60126c
Nick Ingravallo <psychobirdyt@gmail.com>
2026-02-28 19:29:42 -0500

    Continue learning NLH MCCFR algo

--- mccfr/nlh.c
+++ mccfr/nlh.c
@@ -29,8 +29,19 @@ static const int RAISE_PCT[] = { 33, 52, 100 };
 #define STREET_TURN  2
 #define STREET_RIVER 3
 
+typedef struct {
+	float regret_sum[MAX_ACTIONS];
+	float strategy_sum[MAX_ACTIONS];
+	uint64_t key;
+} InfoSet;
+
+typedef struct {
+	uint64_t key;
+	InfoSet infoSet;
+} HashTable;
+
 static uint64_t gto_rng_state;
-HashTable* gto_table = NULL;
+HashTable* table = NULL;
 
 void gto_rng_seed(unsigned int seed) {
 	gto_rng_state = (uint64_t)seed;
@@ -38,13 +49,85 @@ void gto_rng_seed(unsigned int seed) {
 		gto_rng_state = 1;
 }
 
+static uint64_t hash_id(uint64_t key) {
+	key ^= key >> 33;
+	key *= 0xff51afd7ed558ccdLLU;
+	key ^= key >> 33;
+	return key % TABLE_SIZE;
+}
+
 float gto_rng_uniform(void) {
 	uint64_t x = gto_rng_state;
 	x ^= x << 13;
 	x ^= x >> 7;
+
 	x ^= x << 17;
 	gto_rng_state = x;
 	return (float)(x >> 11) / (float)(UINT64_C(1) << 53);
 }
 
+void init_table() {
+	size_t i;
+	
+	if (!table) {
+		table = (HashTable*) malloc(TABLE_SIZE * sizeof(HashTable));
+		if (!table)
+			abort();
+	}
+	for (i = 0; i < TABLE_SIZE; i++)
+		table[i].key = EMPTY_MAGIC;
+}
 
+static uint64_t quantize_cents(float value) {
+	if (value <= 0.0f)
+		return 0;
+	return (uint64_t) (value * 100.0f + 0.5f)
+}
+
+InfoSet* get_or_create_node(uint64_t key) {
+	uint64_t id;
+	unsigned int probes = 0;
+	int i;
+
+	id = hash_id(key);
+	while (table[id].key != EMPTY_MAGIC) {
+		if (table[id].key == key)
+			return &table[id].infoSet;
+
+		id = (id + 1) % TABLE_SIZE;
+		if (++probes >= TABLE_SIZE)
+			return NULL;
+	}
+
+	table[id].key = key;
+	for (i = 0; i < MAX_ACTIONS; i++) {
+		table[id].infoSet.regret_sum[i] = 0;
+		table[id].infoSet.strategy_sum[i] = 0;
+	}
+
+	table[id].infoSet.key = key;
+	return &table[id].infoSet;
+}
+
+void init_flop_state(GameState* state, uint64_t p1_hand, uint64_t p2_hand, uint64_t board, uint64_t present_turn, uint64_t preset_river) {
+	uint8_t legal;
+	float actor_stack;
+	uint64_t seen_cards[6];
+	int num_seen_cards;
+
+	if (state->street < STREET_FLOP || state->street > STREET_RIVER)
+		return 0;
+
+	if (state->facing_bet) {
+		legal |= (1u << 0); //fold
+		if (actor_stack > 0.0f)
+			legal != (1u << 1);
+		{
+			float cents = (state->to_call < actor_stack) ?
+				state->to_call :
+				actor_stack;
+			seen_cards[num_seen_cards] = quantize_cents(c);
+		}
+	}
+
+}
