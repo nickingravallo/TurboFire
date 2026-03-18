f2608f5a59e3fa10f9bdfd6439329b6ca7c9d6ee
Nick Ingravallo <psychobirdyt@gmail.com>
2026-03-17 21:12:12 -0400

    work on tree code

--- src/tree.c
+++ src/tree.c
@@ -23,3 +23,25 @@ void* arena_alloc(Arena* a, size_t size) {
 	a->offset +=aligned_size;
 	return ptr;
 }
+
+bool is_hand_over(GameState* state);
+bool is_street_complete(GameState* state);
+int generate_bet_sizes(GameState* state, int* out_action);
+
+PublicNode* build_public_tree(Arena* arena, GameState state, int num_buckets) {
+	PublicNode* node = (PublicNode*) arena_alloc(arena, sizeof(PublicNode));
+	
+	//terminal state (showdown or fold)
+	if (is_hand_over(&state)) {
+		node->type = NODE_TERMINAL;
+		node->num_childern = 0;
+		return node;
+	}
+
+	//chance state (dealing turn or river)
+	if (is_street_complete(&state)) {
+		node->type = NODE_CHANCE;
+
+
+	}
+}
